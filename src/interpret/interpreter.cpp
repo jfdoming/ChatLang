8eccf79e7047f9d56ffb69b422e97733e30b1b12
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2020-09-01 03:59:34 -0400

    Add interpreter package
    This package is used to execute an AST which was generated by the
    `src/simplify` package. Due to the work done in that package,
    interpreting a program is relatively easy; it just entails walking down
    the AST and executing each node recursively.

---
+++ src/interpret/interpreter.cpp
@@ -0,0 +1,42 @@
+#include "./interpreter.hpp"
+
+#include <iostream>
+
+#include "./environment.hpp"
+#include "../simplify/nodes/print.hpp"
+
+using namespace std;
+
+int interpret(ASTNode *ast) {
+    // We should always have an AST at this point, even for an empty program.
+    if (!ast) {
+        cerr << "Uh-oh! An internal error occurred while interpreting your program. This is probably a bug..." << endl;
+        return -2;
+    }
+
+    Environment env{ast};
+
+    // Hard-coded library functions.
+    auto *print = new PrintNode;
+    auto *println = new PrintNode{true};
+    env.setEffect("print", print->interpret(env));
+    env.setEffect("println", println->interpret(env));
+
+    Value *result = ast->interpret(env);
+    if (result) {
+        if (*result) {
+            cout << result->str() << endl;
+            if (!result->isAutoDelete()) {
+                delete result;
+            }
+        } else {
+            cerr << "Returned an error" << endl;
+            if (!result->isAutoDelete()) {
+                delete result;
+            }
+            return -1;
+        }
+    }
+
+    return 0;
+}
---
+++ src/interpret/interpreter.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "src/simplify/ast_node.hpp"
+
+int interpret(ASTNode *ast);
