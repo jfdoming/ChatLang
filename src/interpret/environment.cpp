f43f14d5bc8eda2f456b8ce6ddda21a34644e964
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2020-09-01 03:59:34 -0400

    Add interpreter package
    This package is used to execute an AST which was generated by the
    `src/simplify` package. Due to the work done in that package,
    interpreting a program is relatively easy; it just entails walking down
    the AST and executing each node recursively.

---
+++ src/interpret/environment.cpp
@@ -0,0 +1,162 @@
+#include "./environment.hpp"
+
+#include <iostream>
+
+#include "src/simplify/ast_node.hpp"
+
+using namespace std;
+
+Environment::Environment(ASTNode *root) {
+    frames.emplace();
+    frameIDs.emplace(root);
+}
+
+Environment::~Environment() {
+    while (!frames.empty()) {
+        for (auto &entry : frames.top()) {
+            delete entry.second;
+        }
+        frames.pop();
+    }
+
+    for (auto &entry : effectFrames) {
+        while (!entry.second.empty()) {
+            delete entry.second.top().second;
+            entry.second.pop();
+        }
+    }
+}
+
+int Environment::invoke(const string &name, Value *params, Value *&value) {
+    if (!frames.size()) {
+        cerr << "Uh-oh! An internal error occurred while interpreting your program. This is probably a bug..." << endl;
+        return -1;
+    }
+
+    Value *fn = nullptr;
+    int retval = get(name, fn);
+    if (retval || !fn) {
+        return retval ? retval : -1;
+    }
+
+    if (fn->getType() != ValueType::FUNCTION) {
+        cerr << "Name \"" << name << "\" is not callable." << endl;
+        return -1;
+    }
+
+    return invoke(fn, params, value);
+}
+
+int Environment::invoke(Value *fn, Value *args, Value *&value) {
+    if (!args && fn->getParams().size()) {
+        cerr << "Expected " << fn->getParams().size() << " arguments but got 0." << endl;
+        return -1;
+    }
+
+    frameIDs.emplace(fn->getFn());
+    frames.emplace();
+
+    if (args) {
+        auto &paramNames = fn->getParams();
+        auto &argValues = args->getValues();
+        if (
+            args->getType() == ValueType::TUPLE
+            && paramNames.size() != 1
+        ) {
+            if (paramNames.size() != argValues.size()) {
+                cerr << "Expected " << paramNames.size() << " arguments but got " << argValues.size() << "." << endl;
+                return -1;
+            }
+            for (size_t i = 0; i < paramNames.size(); ++i) {
+                argValues[i]->markAutoDelete();
+                frames.top()[paramNames[i]] = argValues[i];
+            }
+        } else {
+            if (paramNames.size() != 1) {
+                cerr << "Expected " << paramNames.size() << " arguments but got 1." << endl;
+                return -1;
+            }
+
+            args->markAutoDelete();
+            frames.top()[paramNames[0]] = args;
+        }
+    }
+
+    value = fn->getFn()->interpret(*this, 1);
+    if (!value) {
+        value = new Value{0};
+    }
+
+    for (auto &val : frames.top()) {
+        delete val.second;
+    }
+    frames.pop();
+    frameIDs.pop();
+
+    return 0;
+}
+
+int Environment::get(const string &name, Value *&value) const {
+    if (!frames.size()) {
+        cerr << "Uh-oh! An internal error occurred while interpreting your program. This is probably a bug..." << endl;
+        return -1;
+    }
+
+    auto &currentFrame = frames.top();
+    auto entry = currentFrame.find(name);
+    if (entry == currentFrame.end()) {
+        auto currentEffectFrame = effectFrames.find(name);
+        if (
+            currentEffectFrame == effectFrames.end()
+            || currentEffectFrame->second.empty()
+        ) {
+            cerr << "Name \"" << name << "\" is not defined." << endl;
+            return 1;
+        }
+
+        value = currentEffectFrame->second.top().second;
+    } else {
+        value = entry->second;
+    }
+    return 0;
+}
+
+int Environment::set(const string &name, Value *value) {
+    if (!frames.size()) {
+        cerr << "Uh-oh! An internal error occurred while interpreting your program. This is probably a bug..." << endl;
+        return -1;
+    }
+
+    auto &currentFrame = frames.top();
+    auto entry = currentFrame.find(name);
+    if (entry != currentFrame.end()) {
+        delete entry->second;
+    }
+    value->markAutoDelete();
+    currentFrame[name] = value;
+    return 0;
+}
+
+int Environment::setEffect(const string &name, Value *value) {
+    if (!frameIDs.size()) {
+        cerr << "Uh-oh! An internal error occurred while interpreting your program. This is probably a bug..." << endl;
+        return -1;
+    }
+    
+    // Only do this once we're sure the value will be added to a frame.
+    value->markAutoDelete();
+
+    auto *currentFrameID = frameIDs.top();
+    auto nameFrames = effectFrames.find(name);
+    if (
+        nameFrames != effectFrames.end()
+        && !nameFrames->second.empty()
+        && nameFrames->second.top().first == currentFrameID) {
+        delete nameFrames->second.top().second;
+        nameFrames->second.top().second = value;
+        return 0;
+    }
+
+    effectFrames[name].emplace(currentFrameID, value);
+    return 0;
+}
---
+++ src/interpret/value.hpp
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cinttypes>
+#include <cmath>
+
+class ASTNode;
+
+enum class ValueType {
+    NUMBER, STRING, SYMBOL, FUNCTION, TUPLE, ERROR
+};
+
+class Value {
+        double num;
+        std::string strsym;
+        const ASTNode *fn;
+        std::vector<std::string> fnParams;
+        std::vector<Value *> values;
+        ValueType type;
+        bool autoDelete = false;
+    public:
+        Value() : type{ValueType::ERROR} {}
+        Value(double num) : num{num}, type{ValueType::NUMBER} {}
+        Value(std::string strsym, bool sym = false) : strsym{strsym}, type{sym ? ValueType::SYMBOL : ValueType::STRING} {}
+        Value(const ASTNode *fn, const std::vector<std::string> &params) : fn{fn}, fnParams{params}, type{ValueType::FUNCTION} {}
+        Value(const std::vector<Value *> &values) : values{values}, type{ValueType::TUPLE} {}
+
+        ~Value() {
+            for (auto *value : values) {
+                if (!value->autoDelete) {
+                    delete value;
+                }
+            }
+        }
+
+        ValueType getType() {
+            return type;
+        }
+
+        void markAutoDelete() {
+            autoDelete = true;
+        }
+
+        bool isAutoDelete() const {
+            return autoDelete;
+        }
+
+        bool isFalsy() const {
+            if (type == ValueType::NUMBER) {
+                return num == 0;
+            }
+            if (type == ValueType::STRING) {
+                return strsym.size() == 0;
+            }
+            return false;
+        }
+
+        operator bool() const {
+            return type != ValueType::ERROR;
+        }
+
+        std::string str() const {
+            if (type == ValueType::NUMBER) {
+                return std::to_string(num);
+            }
+            if (type == ValueType::FUNCTION) {
+                return std::to_string(reinterpret_cast<uintptr_t>(fn));
+            }
+            if (type == ValueType::TUPLE) {
+                std::string output;
+                for (auto *value : values) {
+                    output += value->str() + ",";
+                }
+                return output;
+            }
+            return strsym;
+        }
+
+        Value *operator +(const Value &other) const {
+            if (
+                type == ValueType::SYMBOL
+                || other.type == ValueType::SYMBOL
+                || type == ValueType::FUNCTION
+                || other.type == ValueType::FUNCTION) {
+                return new Value;
+            }
+            if (type == ValueType::NUMBER && other.type == ValueType::NUMBER) {
+                return new Value{num + other.num};
+            }
+            return new Value{str() + other.str()};
+        }
+
+        Value *operator -(const Value &other) const {
+            if (type == ValueType::NUMBER && other.type == ValueType::NUMBER) {
+                return new Value{num - other.num};
+            }
+            return new Value;
+        }
+
+        Value *operator *(const Value &other) const {
+            if (type == ValueType::NUMBER && other.type == ValueType::NUMBER) {
+                return new Value{num * other.num};
+            }
+            // TODO string-int multiplication
+            return new Value;
+        }
+
+        Value *operator /(const Value &other) const {
+            if (type == ValueType::NUMBER && other.type == ValueType::NUMBER) {
+                return new Value{num / other.num};
+            }
+            // is there string-int division?
+            return new Value;
+        }
+
+        Value *operator %(const Value &other) const {
+            if (type == ValueType::NUMBER && other.type == ValueType::NUMBER) {
+                return new Value{fmod(num, other.num)};
+            }
+            // is there string-int modulo?
+            return new Value;
+        }
+
+        Value *operator ^(const Value &other) const {
+            if (type == ValueType::NUMBER && other.type == ValueType::NUMBER) {
+                return new Value{pow(num, other.num)};
+            }
+            // is there string-int division?
+            return new Value;
+        }
+
+        const ASTNode *getFn() {
+            return fn;
+        }
+
+        std::vector<std::string>& getParams() {
+            return fnParams;
+        }
+
+        std::vector<Value *>& getValues() {
+            return values;
+        }
+};
